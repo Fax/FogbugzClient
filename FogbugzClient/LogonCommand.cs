d99c9cbb68700ade20ab7c798208c0e0288354c4
Simon Jefford <simon.jefford@fourthhospitality.com>
2012-05-21 15:45:07 +0100

    Implement LogonCommand

---
+++ FogbugzClient/LogonCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Fourth.Tradesimple.Fogbugz
+{
+    public class LogonCommand : FogbugzCommand
+    {
+        public override string FogbugzCommandName
+        {
+            get { return "logon"; }
+        }
+
+        protected override void AddParameters(IDictionary<string, string> parameters)
+        {
+            parameters.Add("email", this.Email);
+            parameters.Add("password", this.Password);
+        }
+
+        public string Email { get; private set; }
+
+        public string Password { get; private set; }
+
+        public LogonCommand(string email, string password)
+        {
+            this.Email = email;
+            this.Password = password;
+        }
+    }
+}
---
+++ FogbugzClientTests/LogonCommandTests.cs
@@ -0,0 +1,31 @@
+﻿using Fourth.Tradesimple.Fogbugz;
+using Should;
+using Xunit;
+
+namespace FogbugzClientTests
+{
+    public class LogonCommandTests
+    {
+        private LogonCommand command = new LogonCommand("simon.jefford@fourthhospitality.com", "password");
+
+        [Fact]
+        public void LogonCommand_has_a_fogbugz_command_name_of_logon()
+        {
+            this.command.FogbugzCommandName.ShouldEqual("logon");
+        }
+
+        [Fact]
+        public void LogonCommand_passes_the_email_in_the_query_string()
+        {
+            string query = this.command.ToQueryString();
+            query.ShouldContain("email=simon.jefford%40fourthhospitality.com");
+        }
+
+        [Fact]
+        public void LogonCommand_passes_the_password_in_the_query_string()
+        {
+            string query = this.command.ToQueryString();
+            query.ShouldContain("password=password");
+        }
+    }
+}
