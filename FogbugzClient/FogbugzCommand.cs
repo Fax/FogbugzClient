f112b6bb578bef70fd9e9bf596d93dc474aefae5
Simon Jefford <simon.jefford@fourthhospitality.com>
2012-05-21 14:10:13 +0100

    Initial commit.
    Basic structure and first test in place

---
+++ FogbugzClient/FogbugzCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fourth.Tradesimple
+{
+    public abstract class FogbugzCommand
+    {
+        public abstract string FogbugzCommandName { get;  }
+    }
+}
---
+++ FogbugzClientTests/CommandTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Fourth.Tradesimple;
+using Should;
+
+namespace FogbugzClientTests
+{
+    class TestCommand : FogbugzCommand
+    {
+        public override string FogbugzCommandName
+        {
+            get { return "test"; }
+        }
+    }
+
+    public class CommandTests
+    {
+        [Fact]
+        public void fogbugz_command_requires_you_to_specify_a_command_name()
+        {
+            var command = new TestCommand();
+            command.FogbugzCommandName.ShouldEqual("test");
+        }
+    }
+}
