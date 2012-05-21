89a7d6c519faaaf9b089b0a1279c4786c8df866b
Simon Jefford <simon.jefford@fourthhospitality.com>
2012-05-21 15:51:35 +0100

    Refactor FogbugzCommand.ToQueryString()

--- FogbugzClient/FogbugzCommand.cs
+++ FogbugzClient/FogbugzCommand.cs
@@ -18,13 +18,25 @@ namespace Fourth.Tradesimple
 
         public string ToQueryString()
         {
+            Dictionary<string, string> paramsDictionary = this.GetParameters();
+            var paramstring = ConvertParametersToQuery(paramsDictionary);
+            string query = string.Format("cmd={0}", this.FogbugzCommandName);
+            return string.Join("&", query, paramstring);
+        }
+
+        private static string ConvertParametersToQuery(Dictionary<string, string> paramsDictionary)
+        {
+            var parameterPairs = paramsDictionary.Select(kvp => string.Format("{0}={1}", kvp.Key, HttpUtility.UrlEncode(kvp.Value)));
+            var paramstring = string.Join("&", parameterPairs);
+            return paramstring;
+        }
+
+        private Dictionary<string, string> GetParameters()
+        {
             Dictionary<string, string> paramsDictionary = new Dictionary<string, string>();
             this.AddParameters(paramsDictionary);
             this.AfterParametersAdded(paramsDictionary);
-            var parameterPairs = paramsDictionary.Select(kvp => string.Format("{0}={1}", kvp.Key, HttpUtility.UrlEncode(kvp.Value)));
-            var paramstring = string.Join("&", parameterPairs);
-            string query = string.Format("cmd={0}", this.FogbugzCommandName);
-            return string.Join("&", query, paramstring);
+            return paramsDictionary;
         }
     }
 }
