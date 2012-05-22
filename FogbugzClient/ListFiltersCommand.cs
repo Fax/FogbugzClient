1c09de5cca2ad358c1cbbad3502c6213adaf53b2
Simon Jefford <simon.jefford@fourthhospitality.com>
2012-05-22 10:39:29 +0100

    ListFiltersCommand has a command name of listFilters

---
+++ FogbugzClient/ListFiltersCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Fourth.Tradesimple.Fogbugz
+{
+    public class ListFiltersCommand : AuthorisedFogbugzCommand
+    {
+        public ListFiltersCommand(string token) : base(token)
+        {
+        }
+
+        public override string FogbugzCommandName
+        {
+            get { return "listFilters"; }
+        }
+
+        protected override void AddParameters(IDictionary<string, string> parameters)
+        {
+        }
+    }
+}
---
+++ FogbugzClientTests/ListFiltersCommandTests.cs
@@ -0,0 +1,16 @@
+﻿using Fourth.Tradesimple.Fogbugz;
+using Should;
+using Xunit;
+
+namespace FogbugzClientTests
+{
+    public class ListFiltersCommandTests
+    {
+        [Fact]
+        public void ListFiltersCommand_has_a_command_name_of_listFilters()
+        {
+            ListFiltersCommand command = new ListFiltersCommand("sometoken");
+            command.FogbugzCommandName.ShouldEqual("listFilters");
+        }
+    }
+}
