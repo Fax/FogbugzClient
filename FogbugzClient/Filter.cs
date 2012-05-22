52dbb571665e1304858f89aa739b42ad0637d0ec
Simon Jefford <simon.jefford@fourthhospitality.com>
2012-05-22 14:34:39 +0100

    Filter class - extracts basic attributes from the returned XML

---
+++ FogbugzClient/Filter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Fourth.Tradesimple.Fogbugz
+{
+    public class Filter
+    {
+        public string FogbugzFilterDescriptor { get; private set; }
+
+        public string Type { get; private set; }
+
+        public string Description { get; private set; }
+
+        public Filter(XElement source)
+        {
+            this.Type = source.Attribute("type").Value;
+            this.Description = source.Value;
+            this.FogbugzFilterDescriptor = source.Attribute("sFilter").Value;
+        }
+    }
+}
---
+++ FogbugzClientTests/FilterTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Linq;
+using Fourth.Tradesimple.Fogbugz;
+using Should;
+using Xunit;
+
+namespace FogbugzClientTests
+{
+    public class FilterTests
+    {
+        private Filter builtinFilter;
+
+        public FilterTests()
+        {
+            var element = new XElement("filter",
+                                new XAttribute("type", "builtin"),
+                                new XAttribute("sFilter", "ex349"),
+                                "My Cases");
+            this.builtinFilter = new Filter(element);
+        }
+
+        [Fact]
+        public void Filter_extracts_the_type_from_an_XElement_containing_some_filter_xml()
+        {
+            this.builtinFilter.Type.ShouldEqual("builtin");
+        }
+
+        [Fact]
+        public void Filter_extracts_the_description_from_an_XElement_containing_some_filter_xml()
+        {
+            this.builtinFilter.Description.ShouldEqual("My Cases");
+        }
+
+        [Fact]
+        public void Filter_extracts_the_fogbugz_descriptor_from_an_XElement_containing_some_filter_xml()
+        {
+            this.builtinFilter.FogbugzFilterDescriptor.ShouldEqual("ex349");
+        }
+    }
+}
