70bbb5c0c9dc39e6d367bd0db9db59a69450e565
Simon Jefford <simon.jefford@fourthhospitality.com>
2012-05-22 14:49:20 +0100

    FilterList - takes an XDocument of returned filters and populates itself

---
+++ FogbugzClient/FilterList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Fourth.Tradesimple.Fogbugz
+{
+    public class FilterList : Collection<Filter>
+    {
+        public FilterList(XDocument filterDoc)
+        {
+            this.AddRange(filterDoc.XPathSelectElements("/response/filters/filter")
+                                   .Select(e => new Filter(e)));
+        }
+
+        public void AddRange(IEnumerable<Filter> filters)
+        {
+            foreach (var filter in filters)
+            {
+                this.Add(filter);
+            }
+        }
+    }
+}
---
+++ FogbugzClientTests/FilterListTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Linq;
+using Fourth.Tradesimple.Fogbugz;
+using Should;
+using Xunit;
+
+namespace FogbugzClientTests
+{
+    public class FilterListTests
+    {
+        private FilterList list;
+
+        public FilterListTests()
+        {
+            XDocument filterDoc = new XDocument(
+                new XElement("response",
+                    new XElement("filters",
+                        new XElement("filter",
+                                        new XAttribute("type", "builtin"),
+                                        new XAttribute("sFilter", "ex349"),
+                                        "My Cases"),
+                        new XElement("filter",
+                                        new XAttribute("type", "saved"),
+                                        new XAttribute("sFilter", "304"),
+                                        new XAttribute("status", "current"),
+                                        "Cases I should have closed months ago"))));
+
+            this.list = new FilterList(filterDoc);
+        }
+
+        [Fact]
+        public void FilterList_extracts_contained_filters_from_the_returned_xml()
+        {
+            this.list.Count.ShouldEqual(2);
+            this.list[0].FogbugzFilterDescriptor.ShouldEqual("ex349");
+            this.list[1].FogbugzFilterDescriptor.ShouldEqual("304");
+        }
+    }
+}
